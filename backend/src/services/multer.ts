efdd6177981c9396776477cc9dcc5d842af5f8dc
Samuel Medeiros <42820519+SamuelMMedeiros@users.noreply.github.com>
2025-10-23 12:50:56 -0300

    Fix transformation syntax in multer service

--- backend/src/services/multer.ts
+++ backend/src/services/multer.ts
@@ -7,8 +7,8 @@ const storage = new CloudinaryStorage({
   params: async (req:any, file:any) => ({
     folder: 'martins-tech',
     allowed_formats: ['jpg','jpeg','png','webp'],
-    transformation: [{ width: 1200, crop: 'limit' })]
-  }
+    transformation: [{ width: 1200, crop: 'limit' }]
+  })
 });
 
 export const upload = multer({ storage, limits: { files: 3 } });
