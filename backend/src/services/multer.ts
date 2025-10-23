d4e6a84463cf23a5786e3429e144130ac412de1b
Samuel Medeiros <42820519+SamuelMMedeiros@users.noreply.github.com>
2025-10-23 12:43:29 -0300

    Fix syntax error in multer.ts params definition

--- backend/src/services/multer.ts
+++ backend/src/services/multer.ts
@@ -4,10 +4,10 @@ import cloudinary from './cloudinary';
 
 const storage = new CloudinaryStorage({
   cloudinary,
-  params: {
+  params: async (req:any, file:any) ({
     folder: 'martins-tech',
     allowed_formats: ['jpg','jpeg','png','webp'],
-    transformation: [{ width: 1200, crop: 'limit' }]
+    transformation: [{ width: 1200, crop: 'limit' })]
   }
 });
 
